1a62a3c6886b590590fb5378b498b7818c17b694
dawood <dawood0963@gmail.com>
2025-08-21 19:07:43 +0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+  return (
+       <main>
+      <header>
+        <a href="" className="logo">Myblog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>    
+  )
+}
+
+export default App
