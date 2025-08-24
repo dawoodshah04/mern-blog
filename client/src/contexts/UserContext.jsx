b46f0e02ed5253e2184c583f2b54aad7d99eed35
dawood shah <dawood0963@gmail.com>
2025-08-24 17:13:29 +0500

    userContext added

---
+++ client/src/contexts/UserContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext({});
+
+export function UserContextProvider({children}){
+
+    const [userInfo, setuserInfo] = useState({});
+
+    return(
+        <UserContext.Provider value={{userInfo,setuserInfo}}>
+        {children}
+        </UserContext.Provider>
+    );
+}
