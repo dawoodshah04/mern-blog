7a8a3a8107bdc975f65b2155f76b8923de81544d
Muhammad Dawood Bukhari <124490766+dawoodshah04@users.noreply.github.com>
2026-01-30 00:56:09 +0500

    Update API_BASE_URL to new production endpoint

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 // export const API_BASE_URL = "https://mern-blog-production-336f.up.railway.app";
-export const API_BASE_URL = "http://15.207.133.235:5000/";
+export const API_BASE_URL = "https://snowboard-flash-heather-viii.trycloudflare.com";
 
