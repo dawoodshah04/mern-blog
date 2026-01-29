e044743ca04339a6ab5fb7217a518d3be98eef17
Muhammad Dawood Bukhari <124490766+dawoodshah04@users.noreply.github.com>
2026-01-30 00:42:27 +0500

    Update API_BASE_URL to aws server

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
 // src/config.js
-export const API_BASE_URL = "https://mern-blog-production-336f.up.railway.app";
+// export const API_BASE_URL = "https://mern-blog-production-336f.up.railway.app";
+export const API_BASE_URL = "http://15.207.133.235:5000/";
 
